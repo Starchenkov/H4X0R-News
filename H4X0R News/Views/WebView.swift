ddf7eaba97047fdd1efb7a64f48f76d07935789d
Starchenkov <Starchenkov@live.ru>
2021-02-16 10:15:29 +0700

    Initial Commit

---
+++ H4X0R News/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by Sergey Starchenkov on 12.11.2020.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeUrlString = urlString {
+            if let url = URL(string: safeUrlString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
